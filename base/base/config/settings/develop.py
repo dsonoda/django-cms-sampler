ff114ead20cc354b5c17b4e95b94c20e81a381c0
園田大輔 <>
2018-09-14 15:07:02 +0900

    add base app

---
+++ base/base/config/settings/develop.py
@@ -0,0 +1,97 @@
+"""
+AWS開発環境設定
+"""
+
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# masterブランチでは False に設定すること！
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('WEB_DATABASE_NAME'),
+        'USER': env('WEB_DATABASE_USER'),
+        'PASSWORD': env('WEB_DATABASE_PASSWORD'),
+        'HOST': env('WEB_DATABASE_HOST'),
+        'PORT': env('WEB_DATABASE_PORT'),
+        'ATOMIC_REQUESTS': False,
+    }
+}
+
+"""
+静的ファイル設定
+"""
+# アプリケーションに紐付かない静的ファイルの置き場所
+# collectstaticコマンド実行済の環境では場合はコメントアウト
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+# )
+# collectstaticコマンド実行時の静的ファイル集約先。
+# STATICFILES_DIRS とは別の値を指定する必要あり。
+# DEBUG=False時はrunserverの静的ファイル自動配信機能が動作しないため、必須設定
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+
+"""
+ログ設定
+"""
+LOGGING = {
+    'version': 1,
+    # 既存のログ設定を無効化しない
+    'disable_existing_loggers': False,
+    # ログフォーマット
+    'formatters': {
+        # AWS開発環境⽤
+        'develop': {
+            'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d %(pathname)s:%(lineno)d %(message)s'
+        },
+    },
+    # ハンドラ
+    'handlers': {
+        # ファイル出⼒⽤ハンドラ
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/{}.log'.format(PROJECT_NAME),
+            'formatter': 'develop',
+        },
+    },
+    # ロガー
+    'loggers': {
+        # ⾃作アプリケーション全般のログを拾うロガー
+        '': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        # Django本体が出すログ全般を拾うロガー
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
+"""
+GUIによるデバッグ表示設定
+"""
+if DEBUG:
+    def show_toolbar(request):
+        return True
+
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
+    MIDDLEWARE += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    }
